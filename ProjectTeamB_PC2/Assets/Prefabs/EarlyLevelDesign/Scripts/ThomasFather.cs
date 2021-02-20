1f732b0a87a7d79f4c02eb50e16676a6c9729241
Perdons22 <davide.perdon22@gmail.com>
2021-02-20 21:09:36 +0100

    First Functioning Tank Engine

---
+++ ProjectTeamB_PC2/Assets/Prefabs/EarlyLevelDesign/Scripts/ThomasFather.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThomasFather : MonoBehaviour
+{
+    public GameObject room1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Room"))
+        {
+            Debug.Log("EccoloIlBastardo");
+            Instantiate(room1, gameObject.transform.position, gameObject.transform.rotation);
+        }
+    }
+}
