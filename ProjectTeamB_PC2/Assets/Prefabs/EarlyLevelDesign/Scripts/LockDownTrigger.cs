8fe804e58f785c7c6d180b406c0747047f48bbf6
Perdons22 <davide.perdon22@gmail.com>
2021-02-20 17:37:26 +0100

    Working LockDown

---
+++ ProjectTeamB_PC2/Assets/Prefabs/EarlyLevelDesign/Scripts/LockDownTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockDownTrigger : MonoBehaviour
+{
+    public GameObject lockDown;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            lockDown.SetActive(true);
+        }
+    }
+
+}
