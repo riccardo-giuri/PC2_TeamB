9b6b7fdc897618776010452b825fe139f7e5ea53
Perdons22 <davide.perdon22@gmail.com>
2021-02-20 21:24:23 +0100

    Thomas the Tank Engine without colliders

--- ProjectTeamB_PC2/Assets/Prefabs/EarlyLevelDesign/Scripts/ThomasFather.cs
+++ ProjectTeamB_PC2/Assets/Prefabs/EarlyLevelDesign/Scripts/ThomasFather.cs
@@ -6,24 +6,34 @@ public class ThomasFather : MonoBehaviour
 {
     public GameObject room1;
 
+    private float spawnRoomLengh;
+    private Vector3 spawnPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnPos = transform.position;
+        spawnRoomLengh = GetComponent<BoxCollider>().size.z;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("SpawnPos" + spawnPos);
+
+        if (transform.position.z >= spawnPos.z + spawnRoomLengh)
+        {
+            spawnPos = transform.position;
+            Instantiate(room1, gameObject.transform.position, gameObject.transform.rotation);
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Room"))
-        {
-            Debug.Log("EccoloIlBastardo");
-            Instantiate(room1, gameObject.transform.position, gameObject.transform.rotation);
-        }
+       //if (other.CompareTag("Room"))
+       //{
+       //    Debug.Log("EccoloIlBastardo");
+       //    Instantiate(room1, gameObject.transform.position, gameObject.transform.rotation);
+       //}
     }
 }
