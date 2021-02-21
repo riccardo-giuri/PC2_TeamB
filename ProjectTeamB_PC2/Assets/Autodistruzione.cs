8c50d8d6c6acbfb899e9426568a256808da3cb0b
Perdons22 <davide.perdon22@gmail.com>
2021-02-21 15:46:09 +0100

    Functioning Hitmarker

--- ProjectTeamB_PC2/Assets/Autodistruzione.cs
+++ ProjectTeamB_PC2/Assets/Autodistruzione.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class Autodistruzione : MonoBehaviour
 {
     public float life;
-    public GameObject hitmarker;
     public PlayerLife myScript;
     public float LifeBonus;
+
+    private HitmarkerBlink blink;
 
     void Start()
     {
         myScript = GameObject.Find("Player").GetComponent<PlayerLife>();
+        blink = GameObject.Find("HitmarkerFather").GetComponent<HitmarkerBlink>();
     }
 
     // Update is called once per frame
@@ -36,13 +38,13 @@ public class Autodistruzione : MonoBehaviour
     {
         if (other.CompareTag("Bullet"))
         {
-            hitmarker.SetActive(true);
+            blink.Blink();
             gameObject.transform.localScale = new Vector3(gameObject.transform.localScale.x + 0.1f, gameObject.transform.localScale.y + 0.1f, gameObject.transform.localScale.z + 0.1f);
             life -= 1;
         }
         if (other.CompareTag("Melee"))
         {
-            hitmarker.SetActive(true);
+            blink.Blink();
             gameObject.transform.localScale = new Vector3(gameObject.transform.localScale.x + 0.1f, gameObject.transform.localScale.y + 0.1f, gameObject.transform.localScale.z + 0.1f);
             life -= life;
         }
