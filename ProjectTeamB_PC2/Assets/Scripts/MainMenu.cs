d51ab07f7c6e1f8deedd6a0d573c71c12c16ed38
Perdons22 <davide.perdon22@gmail.com>
2021-02-22 10:56:46 +0100

    Values Modifications

--- ProjectTeamB_PC2/Assets/Scripts/MainMenu.cs
+++ ProjectTeamB_PC2/Assets/Scripts/MainMenu.cs
@@ -5,40 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public string scene1;
-    public string scene2;
-    public string scene3;
-    public string scene4;
-
-    // Start is called before the first frame update
-    void Start()
+    public void LoadScene(string scene)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    public void PlayScene1()
-    {
-        SceneManager.LoadScene("Prototype13_02");
-    }
-
-    public void PlayScene2()
-    {
-        SceneManager.LoadScene("Persia Scene");
-    }
-
-    public void PlayScene3()
-    {
-        SceneManager.LoadScene("PrototypeLockdown");
-    }
-
-    public void PlayScene4()
-    {
-        SceneManager.LoadScene("PrototypeThomas");
+        SceneManager.LoadScene(scene);
     }
 
     public void CloseGame()
