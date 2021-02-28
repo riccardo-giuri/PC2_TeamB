4492a3ca1a7042457055d9377125f019b0b6cb6f
Perdons22 <davide.perdon22@gmail.com>
2021-02-28 16:42:17 +0100

    Functioning Doors

---
+++ ProjectTeamB_PC2/Assets/EarlyLevelDesign/_Pieces/OpenDoor.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    public GameObject doorRight, doorLeft;
+    public Transform rightOpen, leftOpen, rightClosed, leftClosed;
+    public float speed;
+
+    private bool isOpen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isOpen == true)
+        {
+            doorRight.transform.position = Vector3.Lerp(doorRight.transform.position, rightOpen.position, Time.deltaTime * speed);
+            doorLeft.transform.position = Vector3.Lerp(doorLeft.transform.position, leftOpen.position, Time.deltaTime * speed);
+        }
+        else
+            doorRight.transform.position = Vector3.Lerp(doorRight.transform.position, rightClosed.position, Time.deltaTime * speed);
+            doorLeft.transform.position = Vector3.Lerp(doorLeft.transform.position, leftClosed.position, Time.deltaTime * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        isOpen = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        isOpen = false;
+    }
+}
